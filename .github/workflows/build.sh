b7b518a574f327ae5882a1ac44593334b41795da
shigeyukey <shigge.3.21@gmail.com>
2024-08-17 20:16:34 +0900

    update

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# (cd && \
+#   curl -LO https://github.com/ankitects/mdbook-linkcheck/releases/download/anki-2022-11-17/mdbook.tar.zst && \
+#   tar xaf mdbook.tar.zst)
 (cd && \
-  curl -LO https://github.com/ankitects/mdbook-linkcheck/releases/download/anki-2022-11-17/mdbook.tar.zst && \
-  tar xaf mdbook.tar.zst)
+  curl -LO https://github.com/rust-lang/mdBook/releases/download/v0.4.40/mdbook-v0.4.40-x86_64-unknown-linux-gnu.tar.gz && \
+  tar -xzf mdbook-v0.4.40-x86_64-unknown-linux-gnu.tar.gz)
+
+# mdbook-linkcheck ﾌﾟﾗｸﾞｲﾝをｲﾝｽﾄｰﾙ
+(cd && \
+  curl -LO https://github.com/Michael-F-Bryan/mdbook-linkcheck/releases/download/v0.7.7/mdbook-linkcheck.x86_64-unknown-linux-gnu.zip && \
+   unzip mdbook-linkcheck.x86_64-unknown-linux-gnu.zip )
 
 export PATH="$HOME:$PATH"
 if [ "$CHECK" = "" ]; then
