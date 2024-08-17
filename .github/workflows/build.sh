c174e2be4336507de0c63e7d4c4273ba40ecb15b
shigeyukey <shigge.3.21@gmail.com>
2024-08-17 19:05:00 +0900

    update

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,9 +2,11 @@
 
 (cd && \
   curl -LO https://github.com/ankitects/mdbook-linkcheck/releases/download/anki-2022-11-17/mdbook.tar.zst && \
-  tar xaf mdbook.tar.zst)
+  tar xaf mdbook.tar.zst && \
+  cargo install mdbook-anchors)
 export PATH="$HOME:$PATH"
 if [ "$CHECK" = "" ]; then
     rm $HOME/mdbook-linkcheck
 fi
 mdbook build
+
