15ec535b973ed1a5fa44826d04360ab2e75578d7
shigeyukey <shigge.3.21@gmail.com>
2024-08-17 19:45:30 +0900

    update

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# (cd && \
+#   curl -LO https://github.com/ankitects/mdbook-linkcheck/releases/download/anki-2022-11-17/mdbook.tar.zst && \
+#   tar xaf mdbook.tar.zst)
 (cd && \
-  curl -LO https://github.com/ankitects/mdbook-linkcheck/releases/download/anki-2022-11-17/mdbook.tar.zst && \
-  tar xaf mdbook.tar.zst && \
-  cargo install --git https://github.com/badboy/mdbook-anchors.git)
+  curl -LO https://github.com/rust-lang/mdBook/releases/download/v0.4.40/mdbook-v0.4.40-x86_64-unknown-linux-gnu.tar.gz && \
+  tar -xzf mdbook-v0.4.40-x86_64-unknown-linux-gnu.tar.gz)
 
 export PATH="$HOME:$PATH"
 if [ "$CHECK" = "" ]; then
